14cc7216e682cde93175aa7064ef462bd91225cc
Luka <vujnovacluka89@gmail.com>
2021-07-02 23:05:30 +0200

    Initial Commit

---
+++ Shared/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Shared
+//
+//  Created by Luka Vujnovac on 02.07.2021..
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
